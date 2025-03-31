dd2876723d2d47660f96eb46cca386f5d459cd47
Wendel_dev <tybnkdbj35@gmail.com>
2025-03-31 10:43:52 -0300

    feat: improve ClienteCE class with enhanced error handling and additional form field interactions

--- app/utils/clienteCE.py
+++ app/utils/clienteCE.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 import time
 from utils.cliente import Cliente
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
 class ClienteCE(Cliente):
     def __init__(self,args):
@@ -37,28 +38,12 @@ class ClienteCE(Cliente):
             element_select = WebDriverWait(browser, self.TIME_TO_WAIT).until(
             EC.presence_of_element_located((By.ID, 'NOVA_ATIVIDADE')))
             element_select.click()
-        
-        
-        element_select = WebDriverWait(browser, self.TIME_TO_WAIT).until(
-        EC.presence_of_element_located((By.ID, 'CONTRATO_DATAINICIO0')))
-        element_select.clear()
-        element_select.send_keys(self.data)#data inicio
-        
-        element_select = WebDriverWait(browser, self.TIME_TO_WAIT).until(
-        EC.presence_of_element_located((By.ID, 'CONTRATO_DATAFIM0')))
-        element_select.clear()
-        element_select.send_keys(self.data)#data fim
-        
-        element_select = WebDriverWait(browser, self.TIME_TO_WAIT).until(
-        EC.presence_of_element_located((By.ID, 'NIVEL00')))
-        select = Select(element_select)
-        select.select_by_value("38")#execução
 
         element_select = WebDriverWait(browser, self.TIME_TO_WAIT).until(
         EC.presence_of_element_located((By.ID, 'ATIVIDADEPROFISSIONAL00')))
         select = Select(element_select)
         select.select_by_value("4180")#atividade profissional
-        
+
         element_select = WebDriverWait(browser, self.TIME_TO_WAIT).until(
             EC.presence_of_element_located(
                 (By.ID, 'LABELATUACAO00')
@@ -68,12 +53,12 @@ class ClienteCE(Cliente):
         time.sleep(4)
         xpath = "//div[@id='listaAtividadeEscolherATUACAO00']//li[1]"
         browser.find_element(By.XPATH, xpath).click()
-        
+
         element_select = WebDriverWait(browser, self.TIME_TO_WAIT).until(
         EC.presence_of_element_located((By.ID, 'UNIDADEMEDIDA00')))
         select = Select(element_select)
         select.select_by_value("18924748")#unidade de medida
-        
+
         element_select = WebDriverWait(browser, self.TIME_TO_WAIT).until(
         EC.presence_of_element_located((By.ID, 'QUANTIDADE00')))
         element_select.clear()
@@ -82,15 +67,16 @@ class ClienteCE(Cliente):
         element_select = WebDriverWait(browser, self.TIME_TO_WAIT).until(
         EC.presence_of_element_located((By.ID, "contratante0_ContratantePF")))
         element_select.click() #contratante 
-        
-        
+
         element_select = WebDriverWait(browser, self.TIME_TO_WAIT).until(
         EC.presence_of_element_located((By.ID, 'contratante0_CampoContratantePF')))
         element_select.clear()
         element_select.send_keys(self.cpf)#cpf
-        
 
-        
+        element_select = WebDriverWait(browser, self.TIME_TO_WAIT).until(
+        EC.presence_of_element_located((By.ID, 'session_timeout_clock')))
+        element_select.click()
+
         time.sleep(5)
 
         try:
@@ -147,18 +133,66 @@ class ClienteCE(Cliente):
             browser.switch_to.window(browser.window_handles[0])
         except:
             pass
-        
-        
-        element_select = WebDriverWait(browser, self.TIME_TO_WAIT).until(
-        EC.presence_of_element_located((By.ID, 'CONTRATO_VALOR0')))
-        element_select.clear()
-        element_select.send_keys(self.valor_do_plano)#data
-        
+
         time.sleep(5)
         element_select = WebDriverWait(browser, self.TIME_TO_WAIT).until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, '#evtContratoEnderecoContainerSpecific0 > div.cad_form_cont_campo > input[type=radio]:nth-child(1)')))
         element_select.click() #endereco
+
+        element_select = WebDriverWait(browser, self.TIME_TO_WAIT).until(
+        EC.presence_of_element_located((By.ID, 'CONTRATO_DATA0')))
+        element_select.clear()
+        element_select.send_keys(self.data)#data inicio
+        
+        element_select = WebDriverWait(browser, self.TIME_TO_WAIT).until(
+        EC.presence_of_element_located((By.ID, 'CONTRATO_DATAINICIO0')))
+        element_select.clear()
+        element_select.send_keys(self.data)#data inicio
+        
+        element_select = WebDriverWait(browser, self.TIME_TO_WAIT).until(
+        EC.presence_of_element_located((By.ID, 'CONTRATO_DATAFIM0')))
+        element_select.clear()
+        element_select.send_keys(self.data)#data fim
+
+        element_select = WebDriverWait(browser, self.TIME_TO_WAIT).until(
+        EC.presence_of_element_located((By.ID, 'CONTRATO_VALOR0')))
+        element_select.clear()
+        element_select.send_keys(self.valor_do_plano)#data
+
         time.sleep(5)
+
+        try:
+
+            if self.cep:
+                raise ValueError("CEP está preenchido")
+
+            element_select = WebDriverWait(browser, self.TIME_TO_WAIT).until(
+                EC.presence_of_element_located((By.ID, 'CONTRATO_ENDERECO_CEP0')))
+            element_select.clear()
+            element_select.send_keys(self.cep)  # cep
+
+            element_select = WebDriverWait(browser, self.TIME_TO_WAIT).until(
+                EC.presence_of_element_located((By.ID, 'CONTRATO_ENDERECO_TIPOLOGRADOURO0')))
+            select = Select(element_select)
+            select.select_by_value(self.tipo_de_logradouro)  # tipo_de_logradouro
+
+            element_select = WebDriverWait(browser, self.TIME_TO_WAIT).until(
+                EC.presence_of_element_located((By.ID, 'CONTRATO_ENDERECO_LOGRADOURO0')))
+            element_select.clear()
+            element_select.send_keys(self.logradouro)  # logradouro
+
+            element_select = WebDriverWait(browser, self.TIME_TO_WAIT).until(
+                EC.presence_of_element_located((By.ID, 'CONTRATO_ENDERECO_NUMERO0')))
+            element_select.clear()
+            element_select.send_keys(self.numero)  # numero
+
+            element_select = WebDriverWait(browser, self.TIME_TO_WAIT).until(
+                EC.presence_of_element_located((By.ID, 'CONTRATO_ENDERECO_BAIRRO0')))
+            element_select.clear()
+            element_select.send_keys(self.bairro)  # bairro
+        except:
+            pass
+        
         
         element_select = WebDriverWait(browser, self.TIME_TO_WAIT).until(
         EC.presence_of_element_located((By.ID, "save")))
@@ -166,24 +200,30 @@ class ClienteCE(Cliente):
         time.sleep(10)
 
     def login_crea(self,browser,login,senha) -> None:
-        browser.get(self.URL_LOGIN_CREA)
+        try:
+            logout_info = browser.find_element(By.ID, "logout_info")
+            print("Logout button found:", logout_info)
+        except NoSuchElementException:
+    
+            print("Logout button not found, proceeding to login.")
+            browser.get(self.URL_LOGIN_CREA)
 
-        cpf_input = WebDriverWait(browser, self.TIME_TO_WAIT).until(
-            EC.presence_of_element_located((By.ID, "login"))
-        )
+            cpf_input = WebDriverWait(browser, self.TIME_TO_WAIT).until(
+                EC.presence_of_element_located((By.ID, "login"))
+            )
 
-        senha_input = WebDriverWait(browser, self.TIME_TO_WAIT).until(
-            EC.presence_of_element_located((By.ID, "senha"))
-        )
+            senha_input = WebDriverWait(browser, self.TIME_TO_WAIT).until(
+                EC.presence_of_element_located((By.ID, "senha"))
+            )
 
-        login_button = WebDriverWait(browser, self.TIME_TO_WAIT).until(
-            EC.presence_of_element_located((By.ID, "enviar"))
-        )
+            login_button = WebDriverWait(browser, self.TIME_TO_WAIT).until(
+                EC.presence_of_element_located((By.ID, "enviar"))
+            )
 
-        cpf_input.send_keys(login)
-        senha_input.send_keys(senha)
-        login_button.click()
+            cpf_input.send_keys(login)
+            senha_input.send_keys(senha)
+            login_button.click()
 
-        WebDriverWait(browser, self.TIME_TO_WAIT).until(
-            EC.presence_of_element_located((By.ID, "logout_info"))
-        )
+            WebDriverWait(browser, self.TIME_TO_WAIT).until(
+                EC.presence_of_element_located((By.ID, "logout_info"))
+            )
